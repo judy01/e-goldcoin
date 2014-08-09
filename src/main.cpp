69e1947544b70ca83ba5203a89c6c269c758ecc0
e-goldcoin-team <info@h2o-mobilfunk.de>
2014-08-10 00:35:10 +0200

    Fixed stakeinterest

--- src/main.cpp
+++ src/main.cpp
@@ -998,14 +998,13 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
 }
 */
 
-const int BLOCKS_PER_SUBSIDY_REDUCTION = 2102400; //4 years at 60 second block times
-const int SUBSIDY_REDUCTION_MULTIPLIER = 0.475;
+
 int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
 {
 
     //reduce subsidy by a multiplier of 0.475 every 2102400 blocks or approx 4 years
-    int64_t nSubsidy = nCoinAge * (0.0036842105263 * pow(SUBSIDY_REDUCTION_MULTIPLIER,((pindexBest->nHeight-1)/BLOCKS_PER_SUBSIDY_REDUCTION)+1)) * 33 / (365 * 33 + 8);
-
+    int64_t nSubsidy = nCoinAge * (0.0036842105263 * pow(0.475,((pindexBest->nHeight-1)/2102400)+1)) * 33 / (365 * 33 + 8);
+    printf("GetProofOfStakeReward(): create=%s nCoinAge=%"PRId64" pindexBest->nHeight-1=%"PRId64"\n", FormatMoney(nSubsidy).c_str(), nCoinAge, pindexBest->nHeight-1);
     if (fDebug && GetBoolArg("-printcreation"))
         printf("GetProofOfStakeReward(): create=%s nCoinAge=%"PRId64"\n", FormatMoney(nSubsidy).c_str(), nCoinAge);
 
