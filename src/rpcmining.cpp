e38f6b3764e78902c2bd79f31305a9747a63cac6
e-goldcoin-team <info@h2o-mobilfunk.de>
2014-08-11 04:04:08 +0200

    interestrate added to rpc

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -54,7 +54,8 @@ Value getmininginfo(const Array& params, bool fHelp)
     weight.push_back(Pair("combined",  (uint64_t)nWeight));
     obj.push_back(Pair("stakeweight", weight));
 
-    obj.push_back(Pair("stakeinterest",    (uint64_t)(3684211 * pow(0.475,((pindexBest->nHeight-1)/2102400)+1)))); //todo: changeme when the stakereward algo changes
+    obj.push_back(Pair("stakeinterest",    ValueFromAmount(3684211 * pow(0.475,((pindexBest->nHeight-1)/2102400)+1)))); //todo: changeme when the stakereward algo changes
+    obj.push_back(Pair("interestrate",    ValueFromAmount(3684211 * pow(0.475,((pindexBest->nHeight-1)/2102400)+1)*100)));
     obj.push_back(Pair("testnet",       fTestNet));
     return obj;
 }
@@ -88,7 +89,8 @@ Value getstakinginfo(const Array& params, bool fHelp)
 
     obj.push_back(Pair("weight", (uint64_t)nWeight));
     obj.push_back(Pair("netstakeweight", (uint64_t)nNetworkWeight));
-
+    obj.push_back(Pair("stakeinterest",    ValueFromAmount(3684211 * pow(0.475,((pindexBest->nHeight-1)/2102400)+1))));
+    obj.push_back(Pair("interestrate",    ValueFromAmount(3684211 * pow(0.475,((pindexBest->nHeight-1)/2102400)+1)*100)));
     obj.push_back(Pair("expectedtime", nExpectedTime));
 
     return obj;
