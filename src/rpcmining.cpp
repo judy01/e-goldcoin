5d3940e9568d578635fefdb648351890fcab8fce
E-gold-team <info@h2o-mobilfunk.de>
2014-09-01 04:49:10 +0200

    Revert "fix int overflow in getstakinginfo"
    This reverts commit b378004f14985db3b8385126b588c3f92fde6137.

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -72,7 +72,7 @@ Value getstakinginfo(const Array& params, bool fHelp)
 
     uint64_t nNetworkWeight = GetPoSKernelPS();
     bool staking = nLastCoinStakeSearchInterval && nWeight;
-    uint64_t nExpectedTime = staking ? (GetTargetSpacing(nBestHeight) * nNetworkWeight / nWeight) : 0;
+    int nExpectedTime = staking ? (nTargetSpacing * nNetworkWeight / nWeight) : -1;
 
     Object obj;
 
